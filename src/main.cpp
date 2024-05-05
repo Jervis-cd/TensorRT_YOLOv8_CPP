ebf3764cff457760059f9c0c5831c3995db8a68b
jervis <zhengrui_cd@163.com>
2024-05-05 07:39:07 +0000

    modified:   src/main.cpp 	new file:   workspace/Result0.jpg 	new file:   workspace/Result1.jpg 	new file:   workspace/infer

--- src/main.cpp
+++ src/main.cpp
@@ -35,14 +35,19 @@ yolo::Image cvimg(const cv::Mat &image){
 }
 
 void batch_inference(){
+  // 推理原始图像
   std::vector<cv::Mat> images{cv::imread("images/car.jpg"),cv::imread("images/zand.jpg")};
+  // 加载engine文件，加载失败直接返回
   auto yolo=yolo::load("yolov8n.transd.engine");
   if(yolo==nullptr) return;
 
+  // 将images中元素，逐一执行cvimg函数后，存储在yoloimages中
   std::vector<yolo::Image> yoloimages(images.size());
   std::transform(images.begin(),images.end(),yoloimages.begin(),cvimg);
-  auto batched_result=yolo->forwards(yoloimages);
-  for(int ib = 0;ib<(int)batched_result.size(); ++ib){
+
+  auto batched_result=yolo->forwards(yoloimages);         // 执行inference
+  // 解码模型输出
+  for(int ib=0;ib<(int)batched_result.size();++ib){
     auto &objs=batched_result[ib];
     auto &image=images[ib];
     for(auto &obj:objs){
@@ -53,7 +58,7 @@ void batch_inference(){
 
       auto name=cocolabels[obj.class_label];
       auto caption=cv::format("%s %.2f",name,obj.confidence);
-      int width = cv::getTextSize(caption,0,1,2,nullptr).width+10;
+      int width=cv::getTextSize(caption,0,1,2,nullptr).width+10;
       cv::rectangle(image,cv::Point(obj.left-3,obj.top-33),
                     cv::Point(obj.left+width,obj.top),cv::Scalar(b,g,r),-1);
       cv::putText(image,caption,cv::Point(obj.left, obj.top-5),0,1,cv::Scalar::all(0),2,16);
